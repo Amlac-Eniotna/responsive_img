1431f76d843bc5dc09e5497556b627fc3cb2ce17
Amlac Eniotna <antoine.calma30@gmail.com>
2024-06-15 14:14:37 +0200

    Create header.tsx

---
+++ app/ui/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="bg-gradient-to-b from-slate-200 from-95%">
+      <div className="m-auto w-full max-w-screen-xl p-8">
+        <h1 className="text text-4xl font-black">{"Responsive <img/>"}</h1>
+      </div>
+    </header>
+  );
+}
