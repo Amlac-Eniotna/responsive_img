2ec9f08245dc385b98b94c86ac17c24de4118288
Amlac Eniotna <antoine.calma30@gmail.com>
2024-06-15 14:53:20 +0200

    customise scrollbar

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
       </section>
       <section id="canvas">
         <ScrollArea className="whitespace-nowrap rounded-md border">
-          <div className="flex w-max items-center gap-8 p-4">
+          <div className="flex w-max items-center gap-8 p-4 pb-6">
             <div className="relative h-72 w-96 rounded border border-slate-400 bg-white">
               <p className="absolute mt-4 bg-slate-900/50 px-1 text-white">
                 1920 x 1080
@@ -82,7 +82,7 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <ScrollBar orientation="horizontal" />
+          <ScrollBar orientation="horizontal" className="mx-4 my-1 h-4" />
         </ScrollArea>
       </section>
       <section id="download">
